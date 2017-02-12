14c08361c6b2c30b99da01a8fd3955f0a325d118
Jay McGavren <jay@mcgavren.com>
2017-02-12 15:38:25 -0700

    Update nginx, unicorn config.
    Move app location to /var/www/guestbook.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-pid "/home/deploy/guestbook/pids/unicorn.pid"
-stderr_path "/home/deploy/guestbook/unicorn/unicorn.log"
-stdout_path "/home/deploy/guestbook/unicorn/unicorn.log"
+pid "/var/www/guestbook/pids/unicorn.pid"
+stderr_path "/var/www/guestbook/unicorn/unicorn.log"
+stdout_path "/var/www/guestbook/unicorn/unicorn.log"
 
 listen "/tmp/unicorn.guestbook.sock"
 worker_processes 2
