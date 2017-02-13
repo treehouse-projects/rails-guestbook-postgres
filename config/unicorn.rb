6a3821906dcf07fbb44377b06499d9f2e11c1d95
Jay McGavren <jay@mcgavren.com>
2017-02-12 18:20:28 -0700

    Fix app paths.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-pid "/var/www/guestbook/pids/unicorn.pid"
-stderr_path "/var/www/guestbook/unicorn/unicorn.log"
-stdout_path "/var/www/guestbook/unicorn/unicorn.log"
+pid "/var/www/guestbook/current/pids/unicorn.pid"
+stderr_path "/var/www/guestbook/current/unicorn/unicorn.log"
+stdout_path "/var/www/guestbook/current/unicorn/unicorn.log"
 
 listen "/tmp/unicorn.guestbook.sock"
 worker_processes 2
