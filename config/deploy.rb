e97ba08ff596a489f76fbcb6894e0a965f8cf936
Jay McGavren <jay@mcgavren.com>
2017-02-14 13:40:04 -0700

    Add deployment repo info.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "guestbook"
+set :repo_url, "git@github.com:treehouse-projects/rails-guestbook-postgres.git"
+set :branch, "capistrano"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
