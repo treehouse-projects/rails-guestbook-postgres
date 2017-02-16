fd6675bb85163f65072d424639051f8212827237
Jay McGavren <jay@mcgavren.com>
2017-02-16 14:55:43 -0700

    Fix deployment paths in unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-pid "/var/www/guestbook/current/tmp/pids/unicorn.pid"
-stderr_path "/var/www/guestbook/current/unicorn/unicorn.log"
-stdout_path "/var/www/guestbook/current/unicorn/unicorn.log"
+pid "/home/deploy/guestbook/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
+stdout_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
 
 listen "/tmp/unicorn.guestbook.sock"
 worker_processes 2
