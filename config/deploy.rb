e7b9ebf1a328c85e6c0067ad38bcdd0c6beb765e
Jay McGavren <jay@mcgavren.com>
2017-02-19 13:43:36 -0700

    Link tmp/pids/ directory from shared/.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,8 @@ set :linked_files, [".rbenv-vars"]
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+append :linked_dirs, "tmp/pids"
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
